78bc4cbabcf68dc6c51c2bf18715baed721b5d49
serbalta <salih.773@gmail.com>
2024-11-05 23:43:15 +0100

    env variable

--- backend/src/main/java/com/example/config/CorsConfig.java
+++ backend/src/main/java/com/example/config/CorsConfig.java
@@ -18,7 +18,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000","http://" + appServerIp + ":3000")
+                        .allowedOrigins("http://localhost:3000", "http://" + appServerIp + ":3000")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -1,7 +1,7 @@
 // src/api/auth.js
 import axios from "axios";
 
-const API_URL = `http://${process.env.APP_SERVER_IP}:8080/api/auth/`;
+const API_URL = `http://${process.env.REACT_APP_APP_SERVER_IP}:8080/api/auth/`;
 
 export const registerUser = (userData) => {
   return axios.post(API_URL + "register", userData);
